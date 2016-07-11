e0b63b09f9b12e667d93bf09136fb04c5b12ee60
imam ferianto <iferianto@yahoo.com>
2016-07-12 00:13:27 +0700

    Update database.php

--- protected/config/database.php
+++ protected/config/database.php
@@ -2,9 +2,9 @@
 
 // This is the database connection configuration.
 return array(
-	'connectionString' => 'pgsql:host=localhost;port=5432;dbname=gis',
-    'username' => 'postgres',
-    'password' => 'postgres',
+	'connectionString' => 'pgsql:host=localhost;port=0999;dbname=',
+    'username' => '',
+    'password' => '',
 	'emulatePrepare' => true,
 	'charset' => 'utf8',
 );
